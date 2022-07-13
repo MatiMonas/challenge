391cc180acbe2951d7d125bfcde15cb47b2155c5
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-07-12 21:52:42 -0300

    add(api) test route and test for that route

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,5 +1,9 @@
-import { Router } from 'express';
+import { Router , Request, Response, Next} from 'express';
 
 const router = Router();
+
+router.get('/test', (req: Request, res: Response, next: Next) => {
+  res.send('Hello World!');
+})
 
 export default router;
---
+++ src/tests/routes/test-routes.spec.js
@@ -0,0 +1,28 @@
+
+const db = require('../../db');
+const request = require('supertest');
+const {server} = require('../../api/app');
+const { constants } = require('../../common/utils');
+const sequelize = db.default.sequelize;
+
+
+
+
+describe('Test Routes', () => {
+  beforeEach(async ()=> {
+    await sequelize.sync({force: true})
+  })
+
+  describe('/test', ()=> {
+    it('should return status 200 and "Hello World" string', async () => {
+      const res = await request(server).get('/test')
+      expect(res.statusCode).toBe(200);
+      expect(res.text).toEqual('Hello World!')
+    })
+  })
+
+  afterAll(async () => {
+    await sequelize.sync({ force: true });
+    sequelize.close();
+  });
+})
