e2377a77cedfab9d9f05535eedb61f5a6da98fcc
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-07-13 20:34:45 -0300

    add(index) provitional bulk create of genres

--- index.ts
+++ index.ts
@@ -1,9 +1,25 @@
 import './src/common/env';
 import { server, connectToDB } from './src/api';
+import db from './src/db';
 
+const { Genre } = db;
 const port = process.env.PORT || 3001;
 
 server.listen(port, async () => {
-  await connectToDB();
+  await connectToDB().then(() => {
+    Genre.bulkCreate([
+      { name: 'Action' },
+      { name: 'Adventure' },
+      { name: 'Animation' },
+      { name: 'Comedy' },
+      { name: 'Crime' },
+      { name: 'Documentary' },
+      { name: 'Drama' },
+      { name: 'Family' },
+      { name: 'Fantasy' },
+      { name: 'History' },
+      { name: 'Horror' },
+    ]);
+  });
   console.log(`Server listening at http://localhost:${port}`);
 });
