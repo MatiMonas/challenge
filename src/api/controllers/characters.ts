8a52a96ab0376c6eb8fa4efb2a59c0207e5cd143
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-07-13 14:31:10 -0300

    CRUD init for characters

--- src/api/controllers/characters.ts
+++ src/api/controllers/characters.ts
@@ -1,4 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
+import db from '../../db';
+
+const { Character } = db;
 
 export async function getCharactersController(
   req: Request,
@@ -9,11 +12,54 @@ export async function getCharactersController(
     res.send([
       {
         id: 1,
-        name: 'Mickey Mouse',
+        name: 'Mickey',
+        image: 'https://sm.ign.com/ign_es/screenshot/default/11112_uq7s.jpg',
+        age: 15,
+        weight: 40,
+      },
+      {
+        id: 2,
+        name: 'Donald',
         image:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Mickey_Mouse_%28cropped%29.jpg/220px-Mickey_Mouse_%28cropped%29.jpg',
+          'https://static.wikia.nocookie.net/disney/images/6/6f/Donald_Duck.png',
+        age: 16,
+        weight: 30,
       },
     ]);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function createCharacterController(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    await Character.create(req.body);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function deleteCharacterController(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function patchCharacterController(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
   } catch (err) {
     next(err);
   }
--- src/api/routes/characters/index.ts
+++ src/api/routes/characters/index.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
-import { getCharactersController } from '../../controllers/characters'
+import {
+  getCharactersController,
+  createCharacterController,
+  deleteCharacterController,
+  patchCharacterController
+} from '../../controllers/characters';
 const router = Router()
-
-    /**
+  /**
    * @openapi
    * /characters:
-   *   get:
+   *    get:
    *     summary: Get all characters
    *     tags:
    *       - Character Routes
@@ -15,31 +19,114 @@ const router = Router()
    *         content: {}
    */
   .get('/', getCharactersController)
-    /**
+
+  /**
    * @openapi
    * /characters:
-   *    post:
+   *   post:
    *     summary: Creates a new character
    *     tags:
    *       - Character Routes
-   *    requestBody:
-   *     content:
-   *      application/json:
-   *       schema:
-   *       type: object
-   *      properties:
-   *      name:
-   *      type: string
-   *     description:
-   *    required:
-   *    - name
+   *     parameters:
+   *       - in: body
+   *         name: 
+   *          character
+   *         description: Character object
+   *         schema:
+   *          type: object
+   *          required:
+   *            - name
+   *            - age
+   *          properties:
+   *            name:
+   *              type: string
+   *              example: Mickey
+   *              required: true
+   *            age:
+   *              type: number
+   *              example: 15
+   *              required: true
+   *            weight:
+   *              type: number
+   *              example: 40
+   *            image:
+   *              type: string 
+   *              example: https://sm.ign.com/ign_es/screenshot/default/11112_uq7s.jpg
+   *            history:
+   *             type: string
+   *             example: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+   *     responses:
+   *       201:
+   *         description: Created
+   */
+  .post('/', createCharacterController)
+
+  /**
+   * @openapi
+   * /characters/{id}:
+   *   delete:
+   *    summary: Deletes a character
+   *    tags:
+   *      - Character Routes
+   *    parameters:
+   *      - in: path
+   *        name: id
+   *        description: Character id
+   *        required: true
+   *        schema:
+   *          type: number   *    
    *    responses:
    *      200:
-   *       description: Return
-   *      content: {}
+   *        description: Deleted
+   *        content: {}
    */
+  .delete('/character/:id', deleteCharacterController)
+  
+  /**
+   * @openapi
+   * /characters/{id}:
+   *   patch:
+   *    summary: Updates some propertie of a character
+   *    tags:
+   *      - Character Routes
+   *    parameters:
+   *      - in: path
+   *        name: id
+   *        description: Character id
+   *        required: true
+   *        schema:
+   *          type: number
+   *      - in: body
+   *        name: character
+   *        description: Character object
+   *        schema:
+   *          type: object
+   *          properties:
+   *            name:
+   *              type: string
+   *              example: Mickey
+   *              required: true
+   *            age:
+   *              type: number
+   *              example: 15
+   *              required: true
+   *            weight:
+   *              type: number
+   *              example: 40
+   *            image:
+   *              type: string 
+   *              example: https://sm.ign.com/ign_es/screenshot/default/11112_uq7s.jpg
+   *            history:
+   *             type: string
+   *             example: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+   *    description: Endpoint for template deletion
+   *    responses:
+   *      200:
+   *        description: Deleted
+   *        content: {}
+   */
+  .patch('/character/:id', patchCharacterController)
 
-
-
+  
 
 export default router;
