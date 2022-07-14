8d799ce0af8ca9778bf6670869c249dfe3e8e1b9
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-07-14 17:47:14 -0300

    add(controllers && routes/auth) auth done

--- src/api/controllers/auth.ts
+++ src/api/controllers/auth.ts
@@ -1,1 +1,82 @@
 import { NextFunction, Request, Response } from 'express';
+import { generateAccessToken } from '../../common/jwt';
+import { hashPassword } from '../../common/utils';
+import db from '../../db';
+import bcrypt from 'bcryptjs';
+const { User } = db;
+
+export async function authRegisterController(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const { email, password } = req.body;
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: 'Email and password are required',
+      });
+    }
+
+    if (!regexEmail.test(email)) {
+      return res.status(400).json({
+        message: 'invalid Email',
+      });
+    }
+    const user = {
+      email,
+      password,
+    };
+
+    const oldUser = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (oldUser) {
+      return res.status(400).json({
+        message: 'user already exists',
+      });
+    }
+    await User.create(user);
+    return res.status(201).json({
+      message: 'user created',
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function authLoginController(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if (!user || !checkPassword) {
+      return res.status(400).json({
+        message: 'email or password is incorrect',
+      });
+    }
+
+    const accessToken = generateAccessToken(user);
+    return res.status(200).json({
+      accessToken,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
---
+++ src/api/routes/auth/index.ts
@@ -0,0 +1,67 @@
+import { Router } from 'express';
+import { authLoginController, authRegisterController } from '../../controllers/auth';
+
+const router = Router()
+  /**
+   * @openapi
+   * /auth/register:
+   *   post:
+   *     summary: Creates a new user
+   *     tags:
+   *       - Auth Routes
+   *     parameters:
+   *       - in: body
+   *         name:
+   *          user
+   *         description: User object
+   *         schema:
+   *          type: object
+   *          required:
+   *            - email
+   *            - password
+   *          properties:
+   *            email:
+   *              type: string
+   *              example: usuarioNuevo@gmail.com
+   *              required: true
+   *            password:
+   *              type: string
+   *              example: contraseñaDificil
+   *              required: true
+   *     responses:
+   *       201:
+   *         description: Created
+   */
+   .post('/register', authRegisterController)
+  /**
+   * @openapi
+   * /auth/login:
+   *   post:
+   *     summary: Login for the user
+   *     tags:
+   *       - Auth Routes
+   *     parameters:
+   *       - in: body
+   *         name:
+   *          user
+   *         description: User object
+   *         schema:
+   *          type: object
+   *          required:
+   *            - email
+   *            - password
+   *          properties:
+   *            email:
+   *              type: string
+   *              example: usuarioNuevo@gmail.com
+   *              required: true
+   *            password:
+   *              type: string
+   *              example: contraseñaDificil
+   *              required: true
+   *     responses:
+   *       200:
+   *         description: Created
+   */
+   .post('/login', authLoginController)
+export default router;
