e7cc27f208604b951d6cc90240cbdcd7c3246af0
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-07-14 01:46:30 -0300

    cleaned(tests/test-rotes)

--- src/tests/routes/test-routes.spec.js
+++ src/tests/routes/test-routes.spec.js
@@ -1,28 +1,23 @@
-
 const db = require('../../db');
 const request = require('supertest');
-const {server} = require('../../api');
-const { constants } = require('../../common/utils');
+const { server } = require('../../api');
 const sequelize = db.default.sequelize;
 
-
-
-
 describe('Test Routes', () => {
-  beforeEach(async ()=> {
-    await sequelize.sync({force: true})
-  })
+  beforeEach(async () => {
+    await sequelize.sync({ force: true });
+  });
 
-  describe('/test', ()=> {
+  describe('/test', () => {
     it('should return status 200 and "Hello World" string', async () => {
-      const res = await request(server).get('/test')
+      const res = await request(server).get('/test');
       expect(res.statusCode).toBe(200);
-      expect(res.text).toEqual('Hello World!')
-    })
-  })
+      expect(res.text).toEqual('Hello World!');
+    });
+  });
 
   afterAll(async () => {
     await sequelize.sync({ force: true });
     sequelize.close();
   });
-})
+});
