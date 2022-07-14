49232ade3b359551ef203f86d2b24a3d70c33486
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-07-14 15:01:41 -0300

    add(controllers/movies) validations on genreId and rating in creator function

--- src/api/controllers/movies.ts
+++ src/api/controllers/movies.ts
@@ -1,12 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { Op } from 'sequelize';
-import { movie } from '../../common/interfaces';
 import db from '../../db';
 
 const { Character, Movie, Genre } = db;
 
 interface IMoviesWhere {
-  title?: {[Op.iLike]: string};
+  title?: { [Op.iLike]: string };
   genreId?: string;
 }
 
@@ -18,22 +17,21 @@ export async function movieGetController(
   try {
     let { id, title, genre, orderBy } = req.query;
     id = parseInt(id) as number;
-    genre  = parseInt(genre) as number;
+    genre = parseInt(genre) as number;
     orderBy = orderBy as string;
 
     const where: IMoviesWhere = {};
-    
 
-    if (title) where.title =  {[Op.iLike]: `%${title}%`};
+    if (title) where.title = { [Op.iLike]: `%${title}%` };
     if (genre) where.genreId = genre;
 
     if (!id) {
-      const querySearch ={
+      const querySearch = {
         where,
         attributes: ['id', 'title', 'image', 'releaseDate'],
-        order: /^(asc|desc)$/i.test(orderBy) ? [['rating',orderBy]] : [],
-      }
-      
+        order: /^(asc|desc)$/i.test(orderBy) ? [['rating', orderBy]] : [],
+      };
+
       const movies = await Movie.findAll(querySearch);
 
       if (!movies.length) {
@@ -75,11 +73,20 @@ export async function movieCreatorController(
 ) {
   try {
     let { title, releaseDate, rating, image, genreId } = req.body;
-    genreId = parseInt(genreId);
 
+    genreId = Number(genreId).toFixed(0);
+    rating = Number(rating).toFixed(0);
+    
     if (!title || !releaseDate || !genreId) {
       return res.status(400).send({
         message: 'Missing required parameters',
+      });
+    }
+
+    
+    if (isNaN(genreId) || isNaN(rating)) {
+      return res.status(400).json({
+        message: 'rating and genreId must be an integer number'
       });
     }
 
@@ -87,8 +94,10 @@ export async function movieCreatorController(
 
     await movie.setGenre(genreId);
 
-    return res.status(200).json(movie);
+    return res.status(201).json({ message: 'Movie created' });
   } catch (err) {
+    console.log(err);
+
     next(err);
   }
 }
