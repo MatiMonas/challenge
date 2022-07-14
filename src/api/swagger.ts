efa363e124f575e633d4ca86dba2e3a0f0f1cae6
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-07-13 21:55:26 -0300

    add(api/swagger) doc description

--- src/api/swagger.ts
+++ src/api/swagger.ts
@@ -6,8 +6,13 @@ import path from 'path';
 const swaggerOptions = {
   swaggerDefinition: {
     info: {
-      title: 'Disney API',
-      description: 'Disney API documentation',
+      title: 'Disney API, Alkemy challenge',
+      description: `La api cuenta con los modelos de Character, Movies y Genre. Los generos son creados una vez la base de datos inicializa.      
+      Para el correcto funcionamiento de la API seguir los pasos:
+      
+      1- Crear una película. Tener en cuenta que las películas deben tener un genero asociado. Seguir el ejemplo de la petición MOVIES ROUTES POST.
+      
+      2- Una vez creada la pelicula, ya se puede crear un personaje. Este debe estar relacionado con al menos una película. Seguir el ejemplo de la petición CHARACTERS ROUTES POST.`,
       contact: {
         name: 'Matías Monastirsky',
       },
@@ -15,8 +20,6 @@ const swaggerOptions = {
     },
   },
   defaultModelRendering: 'model',
-
-  
 
   apis: [
     path.resolve(__dirname, './routes/index*'),
