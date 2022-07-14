4ed2f6ff89976489fca40fd358655ffa54f03a82
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-07-14 15:48:10 -0300

    changed file name

--- src/api/controllers/testController.ts
+++ src/api/controllers/testController.ts
@@ -1,9 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 
-export function testController(req: Request, res: Response, next: NextFunction)  {
+export function testController(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
   try {
     res.send('Hello World!');
   } catch (err) {
     next(err);
   }
-};
+}
