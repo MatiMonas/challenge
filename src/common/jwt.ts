f340431a4107f1a7b48e007516972348162116fb
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-07-15 14:22:44 -0300

    changed(common/jwt) jwt expiration

--- src/common/jwt.ts
+++ src/common/jwt.ts
@@ -5,5 +5,5 @@ import jwt from 'jsonwebtoken';
 const { TOKEN_SECRET } = process.env;
 
 export function generateAccessToken(email: string) {
-  return jwt.sign({ email }, TOKEN_SECRET, { expiresIn: '1000h' });
+  return jwt.sign({ email }, TOKEN_SECRET, { expiresIn: '1h' });
 }
