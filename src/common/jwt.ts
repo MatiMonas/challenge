4870e850bf99351e6e48bf72ec174a5f8ac52dad
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-07-14 21:15:51 -0300

    deleted unused variables

--- src/common/jwt.ts
+++ src/common/jwt.ts
@@ -1,9 +1,9 @@
 //@ts-nocheck
-import { Response, NextFunction, Request } from 'express';
+
 import jwt from 'jsonwebtoken';
 
 const { TOKEN_SECRET } = process.env;
 
 export function generateAccessToken(email: string) {
-  return jwt.sign({ email }, TOKEN_SECRET, { expiresIn: '1h' });
+  return jwt.sign({ email }, TOKEN_SECRET, { expiresIn: '1000h' });
 }
